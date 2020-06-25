eb32cfc14426e7b108229ba6e79878e6496941bc
Gah0 <woshishacha3@gmail.com>
2020-06-25 13:17:04 +0800

    build_rootfs: update script and deb

--- build_rootfs.sh
+++ build_rootfs.sh
@@ -1,72 +1,112 @@
 #!/bin/bash
 
+
 INITRD_IMG_OUTPUT_PATH="/home/gah0/rootfs-master"
 ROOTFS_NAME="myinitrd.img"
+CURRENT_DIR=$(cd $(dirname $0); pwd)
+CURRENT_BACK_DIR=$CURRENT_DIR/..
+CURRENT_ROOTFS_DIR=$CURRENT_DIR/rootfs
 
+platform=`uname -m`
 
-sudo apt-get install libncurses5-dev
-cd /busybox
-echo "开始编译busybox"
-sudo make user_defconfig
-sudo make
-sudo make install
-if[ $? -eq 0];then
-	echo "成功编译"
-	cp ./_install/\* ..
-	cd ..
-	rm -r /busybox_1.31.0
+install(){
+	ldconfig -p | grep libncurses5-dev
+	if [ "$?" == 0 ] ;
+		echo "已安装libncurses5-dev"
+	else
+		echo "未安装"
+		if [ $platform='x86_64' ]
+		then
+        	echo 64
+        	sudo apt-get install ncurses
+        	if test "dpkg -l | grep libncurses5" = "1"
+				echo "安装成功"
+			else
+				echo "安装失败"
+			fi
+		else
+        	sudo dpkg -i libncurses5-dev_5.7+20100626-0ubuntu1_i386.deb
+			if test "dpkg -l | grep libncurses5" = "1"
+				echo "安装成功"
+			else
+				echo "安装失败"
+			fi
+		fi
+	fi
+}
 
+mke(){
+	if [ -d $CURRENT_DIR/busybox ]
+		cd /busybox
+		echo "开始编译busybox"
+		sudo make user_defconfig
 
+	if [ $? -eq 0 ]; then
+		sudo make
 
-mkdir dev etc lib proc bin sbin sys
+	if [ $? -eq 0 ]; then
+		sudo make install
 
-echo dir:'pwd'
+	if[ $? -eq 0];then
+		echo "成功编译"
+		cp ./_install/* ..
+		cd ..
+		rm -R /busybox
+}
 
-cat > etc/init.d/rcS << EOF
-echo "======================="
-echo "|  Shortsight System  |"
-echo "======================="
+mkrootfs(){
+	mkdir $CURRENT_DIR/rootfs
+	if test "/$CURRENT_DIR/rootfs"="1"
+		cd rootfs
+		mkdir dev etc lib proc bin sbin sys
+
+cat > $CURRENT_DIR/rootfs/etc/init.d/rcS << EOF
+echo "***********************"
+echo "    Busybox System    *"
+echo "***********************"
 EOF
 chmod +x ./etc/init.d/rcS
 
-cat > etc/profile << EOF
+cat > $CURRENT_DIR/rootfs/etc/profile << EOF
 PATH=/bin:/sbin:/usr/bin:/usr/sbin
 export LD_LIBRARY_PATH=/lib:/usr/lib
-PS1='[\u@\h \W]# '
+PS1='[(busybox)\u@\h \W]# '
 EOF
 
-cat > etc/fstab << EOF
+cat > $CURRENT_DIR/rootfs/etc/fstab << EOF
 proc /proc proc defaults 0 0
 none /tmp ramfs default 0 0
 mdev /dev ramfs default 0 0
 sysfs /sys sysfs default 0 0
 EOF
 
-cat > etc/inittab << EOF
+cat > $CURRENT_DIR/rootfs/etc/inittab << EOF
 ::sysinit:/etc/init.d/rcS
 ::respawn:-/bin/sh
 tty2::askfirst:-/bin/sh
 ::ctrlaltdel:/bin/unmount -a -r
 EOF
 
-cd dev
-sudo cp /dev/console ./dev/ -a
-sudo cp /dev/null ./dev/ -a
-sudo cp /dev/zero ./dev/ -a
-sudo cp /dev/*tty* ./dev/ -a
-sudo cp /dev/ram* ./dev/ -a
+cd $CURRENT_DIR/dev
+sudo cp /dev/console $CURRENT_ROOTFS_DIR/dev/ -a
+sudo cp /dev/null $CCURRENT_ROOTFS_DIR/dev/ -a
+sudo cp /dev/zero $CURRENT_ROOTFS_DIR/dev/ -a
+sudo cp /dev/*tty* $CURRENT_ROOTFS_DIR/dev/ -a
+sudo cp /dev/ram* $CURRENT_ROOTFS_DIR/dev/ -a
+}
 
-dd if=/dev/zero of=${INITRD_IMG_OUTPUT_PATH}/${ROOTFS_NAME}E bs=1024 count=8192
+mkeimg(){
+cd $CURRENT_DIR
+cd ..
+dd if=/dev/zero of=$CURRENT_BACK_DIR bs=1024 count=8192
 mkfs.ext2 -F ${ROOTFS_NAME} 
 sudo mkdir /mnt/initrd
 sudo mount -t ext2 -o loop ${ROOTFS_NAME} /mnt/initrd
-sudo cp rootfs/* /mnt/initrd/ -a
-sudo rm build_rootfs.sh 
-sudo rm my_rootfs.sh
-sudo rm README.md
+sudo cp $CURRENT_DIR/rootfs/* /mnt/initrd/ -a
 sudo umount /mnt/initrd
 
-echo "写入镜像成功"
+if [ $? -eq 0 ]; then
+	echo "写入镜像成功"
 
 sudo cp $ROOTFS_NAME /boot/
 if[ $? -eq 0];then
@@ -75,7 +115,7 @@ if[ $? -eq 0];then
 echo "修改grub文件"
 sudo sed -i 's/#GRUB_HEDDEN_TIMEOUT=0//' /boot/grub.cfg
 sudo cat > /boot/grub.cfg << EOF
-menuentry "Shortsight busybox"{
+menuentry "Busybox-1.23.0"{
 	insmod part_msdos
 	insmod ext2
 
@@ -85,4 +125,7 @@ menuentry "Shortsight busybox"{
 }
 EOF
 
-echo "成功，你自己去重启看看有没有选项吧！"
+sudo update-grub
+if[ $? -eq 0];then
+	echo "成功，你自己去重启看看有没有选项吧！"
+}
